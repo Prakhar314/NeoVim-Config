b84fb840f10ee438dfb67f093524991c533e0dcd
Prakhar Jagwani <prakharjagwani7@gmail.com>
2025-03-18 22:50:30 -0400

    add mappings for git

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- gitsigns
+map("n", "<leader>ph", "<cmd>Gitsigns preview_hunk<CR>", { desc = "GitSigns Preview git hunk" })
+map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "GitSigns Blame git line" })
+map("n", "<leader>]h", "<cmd>Gitsigns next_hunk<CR>", { desc = "GitSigns Next git hunk" })
+map("n", "<leader>[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "GitSigns Previous git hunk" })
+map("n", "<leader>rh", "<cmd>Gitsigns reset_hunk<CR>", { desc = "GitSigns Reset git hunk" })
